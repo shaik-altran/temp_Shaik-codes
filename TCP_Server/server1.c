e60b6c9c9e563e1733ff775bbe7d9724a02a18f6
shaik-altran <fairozshaik4c9@gmail.com>
2022-04-13 11:06:53 +0530

    Simple_Client-server_Socket program

---
+++ TCP_Server/server1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 9002
+
+int main()
+{
+	int server_socket;
+	struct sockaddr_in serverAddr;
+
+	int client_socket;
+	struct sockaddr_in newAddr;
+
+	socklen_t addr_size;
+
+	char buffer[255];
+
+	pid_t child_pid;
+
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(server_socket < 0)
+	{
+		prntf("Error in connection\n");
+		exit(0);
+	}
+	printf("Server Socket is created\n");
+
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port   = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	status = bind(server_socket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+	if(status < 0)
+	{
+		printf("Error in binding\n");
+		exit(0);
+	}
+
+
+
+
